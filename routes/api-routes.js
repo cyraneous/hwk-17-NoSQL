4e8a8a00ffd3ffd0f0ddefe7152d914bdf0bd4c3
Cyrus Hanson <cyrus.hanson@gmail.com>
2020-11-10 17:44:53 -0600

    Edited routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,50 @@
 //db
-const db = require('../models')
-module.exports = (app) => {
+// const db = require('../models')
+const Workout = require("../models/workout.js")
+const app = require('express');
 
-    //////Workout Routes//////
-    //get all
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}, (err, workouts) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(workouts)
-            }
-        });
-    });
-    //add excerise, set id, push to model, set true
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
-        db.Workout.findOneAndUpdate({ _id: params.id },
-            { $push: { excercises: body } },
-            { upsert: true, useFindandModify: false },
-            updatedWorkout => {
-                res.json(updatedWorkout);
-            })
-    });
-    //create new workout
-    app.post('/api/workouts', (req, res) => {
-        db.Workout.create({}).then(newWorkout => {
-            res.json(newWorkout);
-        });
-    });
 
-}
+//////Workout Routes//////
+//get all
+app.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts)
+        }).catch(err => {
+            res.json(err)
+        })
+
+});
+//add excerise, set id, push to model, set true
+app.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.findOneAndUpdate(params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true },
+    ).then(dbWorkouts => {
+        res.json(dbWorkouts)
+    }).catch(err => {
+        res.json(err)
+    })
+
+});
+//create new workout
+app.post('/api/workouts', (req, res) => {
+    Workout.create({}).then(dbWorkouts => {
+        res.json(dbWorkouts);
+    }).catch(err => {
+        res.json(err)
+    })
+});
+
+app.get('/api/workouts/range', (req, res) => {
+    Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.json(err)
+        })
+
+
+})
+
+module.exports = app
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,10 +2,6 @@ const db = require('../models')
 const path = require('path');
 
 module.exports = (app) => {
-    //index
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
     //exercise
     app.get('/exercise', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/exercise.html'));
